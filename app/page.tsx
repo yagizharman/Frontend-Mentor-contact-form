68d8a71b4702982a5ec63808d7086749bf7c1361
yagizharman <yagizharman4biz@gmail.com>
2024-06-30 11:53:02 +0300

    push fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,288 @@
+"use client";
+import InputEx from "@/components/input";
 import Image from "next/image";
+import { useState } from "react";
+import { GrRadial, GrRadialSelected } from "react-icons/gr";
+import { FaRegCheckCircle } from "react-icons/fa";
+
+import { set, z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Textarea } from "@/components/ui/textarea";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/components/ui/use-toast";
+
+enum QueryType {
+  General = "General",
+  Support = "Support",
+  Empty = "",
+}
+const formSchema = z.object({
+  firstName: z.string().min(1, { message: "This field is required" }),
+  lastName: z.string().min(1, { message: "This field is required" }),
+  email: z.string().email({ message: "Please enter a valid email address" }),
+  queryType: z.enum([QueryType.General, QueryType.Support], {
+    message: "Please select a query type",
+  }),
+  message: z.string().min(1, { message: "This field is required" }),
+  consent: z.boolean().refine((value) => value === true, {
+    message: "To submit this form, please consent to being contacted",
+  }),
+});
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const [queryType, setQueryType] = useState<QueryType>(QueryType.Empty);
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      message: "",
+      consent: false,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    toast({
+      title: (
+        <div className="flex flex-row  items-center ">
+          <FaRegCheckCircle className="text-white" />
+          <span className="ml-2 text-white">Message Sent!</span>
         </div>
-      </div>
+      ),
+      description: (
+        <div className="text-white text-xs">
+          Thanks for completing the form. We'll be in touch soon!'
+        </div>
+      ),
+      //Delete the toast after 5 seconds
+      duration: 5000,
+    });
+  }
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="bg-background-prim h-screen w-screen"
+      >
+        <div className=" h-full w-full flex lg:flex-row flex-col justify-center items-center">
+          <div className="bg-white gap-y-4 p-8 lg:p-10 flex flex-col rounded-2xl w-5/12">
+            <div className="text-2xl font-semibold">Contact Us</div>
+            <div className="flex flex-col lg:flex-row gap-y-3 lg:gap-x-3 justify-between">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem className=" w-full max-w-sm items-center ">
+                    <FormLabel
+                      className="text-xs font-semibold text-texts-color  "
+                      htmlFor="firstName"
+                    >
+                      First Name <span className="text-button-prim">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        className=""
+                        type="text"
+                        id="firstName"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="text-xs mt-0" />
+                  </FormItem>
+                )}
+              />
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem className="w-full max-w-sm items-center gap-1.5">
+                    <FormLabel
+                      className="text-xs font-semibold text-texts-color "
+                      htmlFor="lastName"
+                    >
+                      LastName <span className="text-button-prim">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input type="text" id="lastName" {...field} />
+                    </FormControl>
+                    <FormMessage className="text-xs mt-0" />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex  flex-col w-full">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col w-full   ">
+                    <FormLabel
+                      className="text-xs font-semibold text-texts-color "
+                      htmlFor="email"
+                    >
+                      Email Address <span className="text-button-prim">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        id="email"
+                        className="flex w-full flex-col"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="text-xs mt-0" />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex  flex-col w-full">
+              <FormField
+                control={form.control}
+                name="queryType"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col w-full">
+                    <FormLabel
+                      className="text-xs font-semibold text-texts-color "
+                      htmlFor="queryType"
+                    >
+                      Query Type <span className="text-button-prim">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        defaultValue={QueryType.Empty}
+                        onValueChange={field.onChange}
+                      >
+                        <div className=" flex flex-col gap-y-3 lg:flex-row lg:gap-x-3 justify-between  cursor-pointer">
+                          <div
+                            className={`
+                            flex items-center space-x-2 
+                            p-2  w-full text-sm
+                           bg-opacity-10 border-1 border-texts-color2  h-10  rounded-lg
+                            ${
+                              field.value === QueryType.General
+                                ? "bg-button-prim border-18 border-button-prim"
+                                : ""
+                            }
+                            `}
+                          >
+                            <RadioGroupItem
+                              value={QueryType.General}
+                              id="op1"
+                              className={`
+                              col font-normal
+                             
+                              `}
+                            />
+                            <Label
+                              className="font-normal text-texts-color"
+                              htmlFor="op1"
+                            >
+                              General Query
+                            </Label>
+                          </div>
+                          <div
+                            className={`
+                            flex items-center space-x-2 
+                            p-2  w-full text-sm
+                           bg-opacity-10 border-1 border-texts-color2  h-10  rounded-lg
+                            ${
+                              field.value === QueryType.Support
+                                ? "bg-button-prim  border-button-prim border-18"
+                                : ""
+                            }
+                            `}
+                          >
+                            <RadioGroupItem
+                              value={QueryType.Support}
+                              id="op2"
+                            />
+                            <Label
+                              className="font-normal text-texts-color"
+                              htmlFor="op2"
+                            >
+                              Support Request
+                            </Label>
+                          </div>
+                        </div>
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage className="text-xs mt-0" />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="message"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-xs font-semibold text-texts-color">
+                    Message <span className="text-button-prim">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="resize-none outline-none border-texts-color2 border-1 focus:border-button-prim focus:border-18 "
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="flex text-xs justify-start " />
+                </FormItem>
+              )}
+            />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <FormField
+              control={form.control}
+              name="consent"
+              render={({ field }) => (
+                <FormItem className="flex flex-col items-start  rounded-md py-1  lg:py-3">
+                  <FormControl>
+                    <div className=" flex flex-row gap-x-4 items-center">
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                        className="rounded-none "
+                      />
+                      <label
+                        htmlFor="terms"
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70   text-texts-color"
+                      >
+                        I consent to being contacted by the team{" "}
+                        <span className="text-button-prim">*</span>
+                      </label>
+                    </div>
+                  </FormControl>
+                  <FormMessage className="flex text-xs justify-start " />
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              className="bg-button-prim py-2 text-white rounded-lg hover:bg-green-800 "
+            >
+              Submit
+            </Button>
+          </div>
+        </div>
+      </form>
+    </Form>
   );
 }
